98e9fd13de7e655543aa902aeb3419d5afab37ed
Jonatha Cardoso <ojonathacardoso@gmail.com>
2019-09-28 01:39:27 -0300

    Correção do arquivo de execução

--- run.R
+++ run.R
@@ -1,5 +1,4 @@
 require(shiny)
 options(shiny.host = '0.0.0.0')
 options(shiny.port = 8888)
-folder_address <- '~/R/ranalyzer'
-runApp(folder_address, launch.browser=TRUE)
+runApp(launch.browser=TRUE)
